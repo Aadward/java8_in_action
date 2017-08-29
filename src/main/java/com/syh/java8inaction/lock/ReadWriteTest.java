45b1ba56de4ce64ebd4a6a7a908e93c863822c86
沈钰杭 <512563551@qq.com>
2017-08-30 00:10:39 +0800

    修改部分参数

--- src/main/java/com/syh/java8inaction/lock/ReadWriteTest.java
+++ src/main/java/com/syh/java8inaction/lock/ReadWriteTest.java
@@ -6,17 +6,17 @@ public class ReadWriteTest {
     public static void main(String[] args) {
 
         System.out.println("===================ReadWriteLock=================");
-        ReadWriteRW rwrw = new ReadWriteRW();
+        ReadWriteRW rwrw = new ReadWriteRW(1000 * 1000 * 10, 10, 20);
         rwrw.start();
         System.out.println(rwrw.getConclusion());
 
         System.out.println("===================StampedLock=================");
-        StampedRW rw = new StampedRW();
+        StampedRW rw = new StampedRW(1000 * 1000 * 10, 10, 20);
         rw.start();
         System.out.println(rw.getConclusion());
 
         System.out.println("===================Synchronized=================");
-        SynchronizedRW srw = new SynchronizedRW();
+        SynchronizedRW srw = new SynchronizedRW(1000 * 1000 * 10, 10, 20);
         srw.start();
         System.out.println(srw.getConclusion());
     }
