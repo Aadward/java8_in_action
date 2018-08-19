f4dfab20ca66cd2d0893380262f4b1efbd2d3119
沈钰杭 <512563551@qq.com>
2018-08-19 13:03:43 +0800

    kafka stream

--- kafka/src/main/java/com/syh/kafka/ConsumerDemo.java
+++ kafka/src/main/java/com/syh/kafka/ConsumerDemo.java
@@ -1,15 +1,22 @@
 package com.syh.kafka;
 
 import com.google.common.collect.ImmutableMap;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ConsumerDemo {
+
+    static final String topic = "test";
 
     static Map<String, Object> props = new ImmutableMap.Builder<String, Object>()
             .put("bootstrap.servers", "localhost:9092")
@@ -20,17 +27,33 @@ public class ConsumerDemo {
             .put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
             .build();
 
-    public static KafkaConsumer<String, String> create() {
-        return new KafkaConsumer<>(props, new StringDeserializer(), new StringDeserializer());
+    public static KafkaConsumer<String, Long> create() {
+        return new KafkaConsumer<>(props, new StringDeserializer(), new LongDeserializer());
     }
 
     public static void main(String[] args) {
-        KafkaConsumer<String, String> consumer = create();
+        KafkaConsumer<String, Long> consumer = create();
 
         try {
-            consumer.subscribe(Collections.singletonList("test"));
+            Collection<TopicPartition> topicPartitions = consumer.partitionsFor(topic)
+                    .stream()
+                    .map(info -> new TopicPartition(topic, info.partition()))
+                    .collect(Collectors.toList());
+
+            consumer.subscribe(Collections.singletonList(topic), new ConsumerRebalanceListener() {
+                @Override
+                public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+
+                }
+
+                @Override
+                public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+                    // 重置topic中所有partition的offset，读取所有的message
+                    consumer.seekToBeginning(topicPartitions);
+                }
+            });
             while (true) {
-                ConsumerRecords<String, String> records = consumer.poll(10000);
+                ConsumerRecords<String, Long> records = consumer.poll(10000);
                 records.iterator().forEachRemaining(record -> System.out.println("key=" + record.key() + ",value=" + record.value()));
             }
         } catch (WakeupException e) {
--- kafka/src/main/java/com/syh/kafka/ProducerDemo.java
+++ kafka/src/main/java/com/syh/kafka/ProducerDemo.java
@@ -8,6 +8,7 @@ import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.errors.AuthorizationException;
 import org.apache.kafka.common.errors.OutOfOrderSequenceException;
 import org.apache.kafka.common.errors.ProducerFencedException;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Map;
@@ -19,16 +20,16 @@ public class ProducerDemo {
             .put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "test-transaction-id")
             .build();
 
-    public static KafkaProducer<String, String> create() {
-        return new KafkaProducer<>(props, new StringSerializer(), new StringSerializer());
+    public static KafkaProducer<String, Long> create() {
+        return new KafkaProducer<>(props, new StringSerializer(), new LongSerializer());
     }
 
     public static void main(String[] args) {
-        KafkaProducer<String, String> producer = create();
+        KafkaProducer<String, Long> producer = create();
         producer.initTransactions();
         try {
             producer.beginTransaction();
-            producer.send(new ProducerRecord<>("test", "1", "1"));
+            producer.send(new ProducerRecord<>("test", "1", 1L));
         } catch (ProducerFencedException | OutOfOrderSequenceException | AuthorizationException e) {
             producer.close();
         } catch (KafkaException e) {
